3e2794e50b313f7c0e0f2bcc5e6f04ec88979212
“Naty-S” <na7a5cha010@gmail.com>
2025-08-06 14:37:41 -0400

    add max len to chapter choices

--- programs/branching_comics/src/state/chapter.rs
+++ programs/branching_comics/src/state/chapter.rs
@@ -8,6 +8,7 @@ pub struct Chapter {
   pub mint: Pubkey,
   pub next: Option<Pubkey>,
   pub start: bool,          // Determines if is the start chapter in a branch/path
+  #[max_len(10)]
   pub choices: Vec<Pubkey>,
   pub price: u64,
   pub comic_bump: u8,
